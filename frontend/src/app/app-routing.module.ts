d4100e44fe4b3d09e8d2d2c24dfc9ea8b964fa9f
Oleh <33325458+MatviyO@users.noreply.github.com>
2020-08-20 12:31:27 +0300

    d

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,13 +1,23 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 import {DashboardComponent} from "./dashboard/dashboard.component";
+import {CalculatorComponent} from "./calculator/calculator.component";
+import {NotesComponent} from "./notes/notes.component";
+import {LayoutComponent} from "./layout/layout.component";
 
 const routes: Routes = [
-  {path: '', component: DashboardComponent}
+  {
+    path: '', component: LayoutComponent, children: [
+      {path: '', component: DashboardComponent},
+      {path: 'calculator', component: CalculatorComponent},
+      {path: 'notes', component: NotesComponent}
+    ]
+  }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,16 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { NotesComponent } from './notes/notes.component';
+import { CalculatorComponent } from './calculator/calculator.component';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    NotesComponent,
+    CalculatorComponent,
+    LayoutComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
